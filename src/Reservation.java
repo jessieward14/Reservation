376bd9a622cb49217c8602571aaf9ec2620e9045
jessieward <jessica_ward25@milton.edu>
2023-10-11 10:56:39 -0400

    Initial commit

---
+++ src/Reservation.java
@@ -0,0 +1,26 @@
+public class Reservation {
+
+
+    //this will help me make reservation objects
+     public int numPeople;
+     public double time;
+
+     int tableNumber;
+     public String name;
+
+     public int priority;
+     //helpful for finding current time
+    // System.currentTimeMillis();
+
+    public Reservation(int nnumPeople, double ttime, int ppriority, String nname, int ttableNumber){
+        numPeople=nnumPeople;
+        time = ttime;
+        name = nname;
+        priority = ppriority;
+        tableNumber = ttableNumber;
+
+        System.out.println (name + " booked a reservation for" + numPeople + "people at " + time + "o'clock for table " + tableNumber +"." );
+
+    }
+
+}
---
+++ src/ReservationSystem.java
@@ -0,0 +1,14 @@
+public class ReservationSystem {
+    public static void main(String[] args) {
+       ReservationSystem jarvis = new ReservationSystem();
+    }
+
+
+    public ReservationSystem(){
+        Reservation gavin = new Reservation(1, 1, 1, "Jessie", 1);
+        gavin.toString();
+
+
+    }
+
+}
